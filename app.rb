224b23d4e72d644fd2ad18b15e1119ad0a6e8a7b
deeptiwari <deeptiwariwork@gmail.com>
2020-05-21 10:00:54 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,17 +9,15 @@ get "/" do
   erb :home
 end
 
+
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
+
 #get "/contacts" do
 #  @contacts = Contact.all
 #  erb :index
 #end
-get "/contacts" do
- @contacts = Contact.all
-  erb :index
-end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
